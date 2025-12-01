2b29a6c004cac6e6c4689298cceaf7b9a189eae4
Younes Ghorbany <younes@ghorbany.dev>
2025-12-01 20:59:16 +0330

    Added lesson 10 source files

--- counter-app/src/playground.ts
+++ counter-app/src/playground.ts
@@ -1,7 +1,46 @@
-//Implicit type
-let review = 5;
-let greeting = "Hello"; //string
+/*Primitive Types:
+  number
+  string
+  boolean
+  null
+  undefined
+  void
+  bigint
+  symbol
+*/
 
-//Explicit type
+//number
 let age: number = 33;
+age = 25;
+
+//string
 let fullname: string = "Younes Ghorbany";
+fullname = "Ali";
+
+let hello: string;
+hello = "World";
+
+//boolean
+let isActive: boolean = true;
+isActive = false;
+
+//null && undefined
+let car;
+console.log(car);
+
+let currentUser = { name: "Younes" };
+
+//logout
+currentUser = null;
+
+let x: null;
+let y: undefined;
+
+x = null; //OK
+// x = 12; //Error
+
+y = undefined; //OK
+// y = "Hello"; //Error
+
+let user: string | null = null;
+user = "Younes";
