88589be0080f4820017cc07fd82ce05e85044939
Younes Ghorbany <younes@ghorbany.dev>
2026-03-05 16:20:13 +0330

    Added lesson 22 source files

--- counter-app/src/script.ts
+++ counter-app/src/script.ts
@@ -1,3 +1,19 @@
-const incrementBtn = document.getElementById("incBtn");
+// Type Assertion
 
-console.log("Ghorbany.DEV 🚀");
+// const incrementBtn = document.getElementById("incBtn");
+// const incrementBtn : HTMLButtonElement = document.getElementById("incBtn");
+
+//INFO Button Elements
+const incrementBtn = document.getElementById("incBtn") as HTMLButtonElement;
+const decrementBtn = document.getElementById("decBtn") as HTMLButtonElement;
+const resetBtn = document.getElementById("resetBtn") as HTMLButtonElement;
+
+// Angle Bracket -> Type Assertion
+// const incBtn = <HTMLButtonElement> document.getElementById("incBtn");
+
+//INFO Div Elements
+const displayValue = document.getElementById("displayValue") as HTMLDivElement;
+const countSlot = document.getElementById("countSlot") as HTMLDivElement;
+
+// Type Assertion -> Not NULL (!)
+// const incBtn = document.getElementById("incBtn")!;
